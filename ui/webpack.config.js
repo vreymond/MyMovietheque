1024c66e062a7c33eb48a09d2a513aed73fda510
vreymond <valentinreymond@gmail.com>
2020-05-02 21:10:08 +0200

    [Webpack/webpack@feature] Webpack config file initialization

---
+++ ui/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: "./index.js"
+    },
+    target: "node",
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: {
+                    laoder: "babel-laoder",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
