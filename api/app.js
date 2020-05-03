5093726c73943288ecb4c7ddf59bde613dd8eb5f
vreymond <valentinreymond@gmail.com>
2020-05-03 09:33:06 +0200

    [Backend/app@feature] Adding helmet middlewares for http response headers

--- api/app.js
+++ api/app.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
-const app = express()
+const app = express();
 const PORT = 8080;
 
 app.use(cors());
+app.use(helmet());
 
 app.get('/', function (req, res) {
-  res.send('My Movietheque API')
+  res.send('My Movietheque API');
 })
 
 app.listen(PORT, function () {
-  console.log(`MyMovietheque API is listening on port ${PORT}`)
+  console.log(`MyMovietheque API is listening on port ${PORT}`);
 })
