edc5a8e4377e29eb39d1c45278b09aaa574ba9fc
vreymond <valentinreymond@gmail.com>
2020-05-14 08:55:08 +0200

    [Movie-controller/movie@feature] Creating first method in movie controller

--- api/src/controllers/movie.js
+++ api/src/controllers/movie.js
@@ -1,3 +1,29 @@
 const mongoose = require('mongoose');
 
 const Movie = require('../models/movie');
+
+exports.add_movie = (req, res, next) => {
+    const movie = new Movie({
+        _id: new mongoose.Types.ObjectId(),
+        title: req.body.title
+    });
+
+    movie
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Created movie successfully',
+                createdMovie: {
+                    _id: result._id,
+                    title: result.title
+                }
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
+}
